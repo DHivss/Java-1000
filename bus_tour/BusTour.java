f651115e7d5d51ea85942ab4d70af26aef50be0c
DHivss <toby.h@mail.ru>
2022-10-17 15:06:58 +0300

    bus_tour

--- bus_tour/BusTour.java
+++ bus_tour/BusTour.java
@@ -5,29 +5,27 @@ import java.util.*;
 
 public class BusTour {
     public static void main(String[] args) throws IOException {
-        ArrayList<String> data= new ArrayList<>(); // Исходные данные построчно
-        ArrayList<Integer> heightBridge = new ArrayList<>(); // Высота всех мостов
-        int heightBus = 437; // Высота автобуса
-        int numberBridge = 0; // Номер моста по умолчанию
-        String result; // Результат решения задачи
-        FileReader file = new FileReader("input.txt");
-        Scanner sc = new Scanner(file);
-        while (sc.hasNextLine()){
-            data.add(sc.nextLine());
-        }
-        StringTokenizer st = new StringTokenizer(data.get(1), " ");
-        while (st.hasMoreTokens()){
-            heightBridge.add(Integer.valueOf(st.nextToken()));
-        }
-        for(int i = 0; i < heightBridge.size(); i++){
-            if(heightBridge.get(i) <= heightBus){
-                numberBridge = i + 1;
-                break;
+        try {
+            FileOutputStream fos = new FileOutputStream("output.txt");
+            FileReader fr = new FileReader("input.txt");
+            Scanner n = new Scanner(fr);
+            String[] str = n.nextLine().split(" ");
+            Integer[] arr = new Integer[str.length];
+            int bus_height = 437, f = 1;
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.valueOf(str[i]);
+                if (bus_height >= arr[i]) {
+                    fos.write(("Crash " + String.valueOf(i+1)).getBytes());
+                    f++;
+                }
             }
+            n.close();
+            if (f == 1) {
+                fos.write("No crash".getBytes());
+            }
+            fos.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
-        result = (numberBridge == 0) ? "No crash" : "Crash " + numberBridge;
-        FileWriter fileOut = new FileWriter("output.txt");
-        fileOut.write(result);
-        fileOut.close();
     }
 }
